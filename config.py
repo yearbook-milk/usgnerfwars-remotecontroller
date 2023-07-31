193122328c093bd5e38bf2e0c8dd01cc4bfc0657
yearbook-milk <throwaway0x069420@gmail.com>
2023-07-31 14:51:38 -0400

    added new servo control library and config file for such library

--- config.py
+++ config.py
@@ -2,11 +2,14 @@
 # HARDWARE SOFTWARE INTERFACE SETTINGS:
 pin_config = {                                      # These control which pins on the RPi do what.
     
-"leftPin": 12,
-"rightPin": 33,
-"yawPin": 32,
+"leftPin": 18,
+"rightPin": 13,
+"yawPin": 12,
 "afterSpdCmdDelay": 0,                              # This should be set to 0, in order to make the pitch and yaw cmds nonblocking.
-"pulse_freq": 50                                    # Pulse frequency, in hZ. 
-    
+"pulse_freq": 50,                                    # Pulse frequency, in hZ. 
+"pinsToSet": "leftPin rightPin yawPin"
+
 }
 
+
+# If you are using this client in remote mode and not running this on the RPi, simply leave this as default and start in R mode 
